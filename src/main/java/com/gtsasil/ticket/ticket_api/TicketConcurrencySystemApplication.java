7fd568ea6a6866323a973087bffd8d263075a9e2
gleisson.silva <gleisson.silva@grupoamper.com>
2026-01-20 15:06:08 +0000

    first commit

---
+++ src/main/java/com/gtsasil/ticket/ticket_api/TicketConcurrencySystemApplication.java
@@ -0,0 +1,13 @@
+package com.gtsasil.ticket.ticket_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketConcurrencySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketConcurrencySystemApplication.class, args);
+	}
+
+}
